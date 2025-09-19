d8d2aa2b5a39c0d8fda80658d4d43ec99b366d11
RanaUniverse <RanaUniverse321@gmail.com>
2025-09-19 22:44:11 +0530

    a simple example of code to use flask website

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+flask --app main run --host=0.0.0.0 --debug
+```
+
+Thsi upper command in venv need to check my website 
---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template_string("<h1>This is Home Page</h1>")
