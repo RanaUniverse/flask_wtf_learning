db80ddcb5250ea7b25334d27ee32263a0fba4263
RanaUniverse <RanaUniverse321@gmail.com>
2025-09-22 09:18:37 +0530

    making the dropdown options with its link easily with url_for() and the functions name?

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/dropdown/<item_category>")
+def show_category_dropdown(item_category: str):
+    return (
+        f"<h3>You Have Selected The Category of:</h3> "
+        f"<br> "
+        f"<h1> {item_category} </h1>"
+    )
